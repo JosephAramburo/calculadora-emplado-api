a10b53dab95900c6feff4b2a8fc2158bfb8265bb
DESKTOP-7UMHSG3\deyvi <deyvid1913@gmail.com>
2022-10-04 10:09:46 -0600

    [ADD] agregar archivos de ambientes e instalar librerias requeridas

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError       = require('http-errors');
+var express           = require('express');
+var path              = require('path');
+var cookieParser      = require('cookie-parser');
+var logger            = require('morgan');
+var dotenv            = require('dotenv');
+var cors              = require('cors');
+var moment            = require('moment-timezone');
+
+// initialize configuration
+dotenv.config({
+  path: path.join(__dirname,`${process.env.NODE_ENV || ''}.env`)
+});
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+//*******************TIME ZONE
+moment.tz.setDefault("America/Mazatlan");
+//****************************
+
+//************************CORS
+app.use(cors('*'))
+//****************************
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +35,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: `Calculadora API modo: ${process.env.PRODUCTION == 'true' ? 'Producción' : 'Desarrollo' }` });
 });
 
 module.exports = router;
